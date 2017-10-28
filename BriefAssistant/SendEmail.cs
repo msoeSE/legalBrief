f967e7bfe9c0880b46ed98037f5c95dab5fa29da
jacobgralinski <jacobgralinski@gmail.com>
2017-10-28 15:00:13 -0500

    Created a legal brief gmail account. Added a parameter for the file path. For now, the password is hidden in a text file that will be passed around through team members but not on GItHub.

--- BriefAssistant/SendEmail.cs
+++ BriefAssistant/SendEmail.cs
@@ -10,27 +10,28 @@ namespace BriefAssistant
 {
     public class SendEmail
     {
-        public SendEmail(string user_email)
+        public SendEmail(string userEmail, string filePath)
         {
-            string file = "legalbrief.docx";
+            string password = System.IO.File.ReadAllText("password.txt");
+            
             // Create  the file attachment for this e-mail message.
-            Attachment document = new Attachment(file, MediaTypeNames.Application.Octet);
+            Attachment document = new Attachment(filePath, MediaTypeNames.Application.Octet);
             // Add time stamp information for the file.
             ContentDisposition disposition = document.ContentDisposition;
-            disposition.CreationDate = System.IO.File.GetCreationTime(file);
-            disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
-            disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+            disposition.CreationDate = System.IO.File.GetCreationTime(filePath);
+            disposition.ModificationDate = System.IO.File.GetLastWriteTime(filePath);
+            disposition.ReadDate = System.IO.File.GetLastAccessTime(filePath);
 
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
             {
                 EnableSsl = true,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("username", "password") //Username and password of the sender
+                Credentials = new NetworkCredential("legalbriefassistant@gmail.com", password) //Username and password of the sender
             };
 
             MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("sender@email.com");
-            mailMessage.To.Add(user_email); //To be filled in with the users email.
+            mailMessage.From = new MailAddress("legalbriefassistant@gmail.com");
+            mailMessage.To.Add(userEmail); //To be filled in with the users email.
             mailMessage.Body = "Attached is your legal brief draft.";
             mailMessage.Subject = "Your Legal Brief Draft";
             mailMessage.Attachments.Add(document);
