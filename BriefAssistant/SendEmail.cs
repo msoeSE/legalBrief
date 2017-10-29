05d5ce3361556ced068047248ea40e10069d4220
jacobgralinski <jacobgralinski@gmail.com>
2017-10-29 11:50:21 -0500

    Made the changes Cal had commented on.

--- BriefAssistant/SendEmail.cs
+++ BriefAssistant/SendEmail.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 using System.Net.Mime;
-using System.Threading.Tasks;
 
 namespace BriefAssistant
 {
     public class SendEmail
     {
-        public SendEmail(string userEmail, string filePath)
+        private SmtpClient client;
+        private string password;
+
+        public SendEmail()
         {
-            string password = System.IO.File.ReadAllText("password.txt");
-            
+            password = System.IO.File.ReadAllText("password.txt");
+
+            client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("legalbriefassistant@gmail.com", password) //Username and password of the sender
+            };
+        }  
+
+        /// <summary>
+        /// This method sends an email to a given user with the legal brief attached to it.
+        /// </summary>
+        /// <param name="userEmail">The address of the email being sent</param>
+        /// <param name="filePath">The path for the legal brief draft</param>
+        public void SendUserEmail(string userEmail, string filePath)
+        {
             // Create  the file attachment for this e-mail message.
             Attachment document = new Attachment(filePath, MediaTypeNames.Application.Octet);
             // Add time stamp information for the file.
@@ -22,13 +36,6 @@ namespace BriefAssistant
             disposition.ModificationDate = System.IO.File.GetLastWriteTime(filePath);
             disposition.ReadDate = System.IO.File.GetLastAccessTime(filePath);
 
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
-            {
-                EnableSsl = true,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("legalbriefassistant@gmail.com", password) //Username and password of the sender
-            };
-
             MailMessage mailMessage = new MailMessage();
             mailMessage.From = new MailAddress("legalbriefassistant@gmail.com");
             mailMessage.To.Add(userEmail); //To be filled in with the users email.
@@ -36,6 +43,6 @@ namespace BriefAssistant
             mailMessage.Subject = "Your Legal Brief Draft";
             mailMessage.Attachments.Add(document);
             client.Send(mailMessage);
-        }  
+        }
     }
 }
