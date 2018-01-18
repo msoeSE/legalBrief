b51c64236126e79297830548958ba57a708037a8
calharden <calharden@gmail.com>
2018-01-17 23:55:28 -0600

    Fixed issue with mapping profile

--- BriefAssistant/MappingProfile.cs
+++ BriefAssistant/MappingProfile.cs
@@ -12,9 +12,15 @@ namespace BriefAssistant
     {
         public MappingProfile()
         {
-            CreateMap<BriefInfo, BriefDto>().ReverseMap();
-            CreateMap<ContactInfo, ContactInfoDto>().ReverseMap();
-            CreateMap<CircuitCourtCase, CaseDto>().ReverseMap();
+            CreateMap<BriefInfo, BriefDto>(MemberList.None).ReverseMap();
+            CreateMap<ContactInfo, ContactInfoDto>(MemberList.None)
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Address.City))
+                .ForMember(dest => dest.State, opt => opt.MapFrom(src => src.Address.State))
+                .ForMember(dest => dest.Zip, opt => opt.MapFrom(src => src.Address.Zip))
+                .ForMember(dest => dest.Street, opt => opt.MapFrom(src => src.Address.Street))
+                .ForMember(dest => dest.Street2, opt => opt.MapFrom(src => src.Address.Street2))
+                .ReverseMap();
+            CreateMap<CircuitCourtCase, CaseDto>(MemberList.None).ReverseMap();
 
             CreateMap<BriefDto, BriefListItem>();
         }
