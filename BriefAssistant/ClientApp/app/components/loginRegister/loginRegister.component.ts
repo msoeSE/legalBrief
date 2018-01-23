936acd486b1443b006468c1221f1a9deada0fba0
trevordickson96 <trevordickson96@gmail.com>
2018-01-23 17:08:56 -0600

    Updated the login and registration htmls to include feedback for the user

--- BriefAssistant/ClientApp/app/components/loginRegister/loginRegister.component.ts
+++ BriefAssistant/ClientApp/app/components/loginRegister/loginRegister.component.ts
@@ -12,31 +12,40 @@ import { RegistrationRequest } from "../../models/RegistrationRequest";
 export class LoginRegisterComponent {
     private loginModel = new LoginRequest();
     private registerModel = new RegistrationRequest();
+    public showRegisterSuccessDiv: boolean = false;
+    public showLoginUnauthorizedDiv: boolean = false;
 
     constructor(
 		private readonly http: Http,
-		private readonly router: Router
+        private readonly router: Router
     ) { }
 
     onLoginSubmit(form: NgForm) {
+        this.showLoginUnauthorizedDiv = false;
         var body = JSON.stringify(this.loginModel);
         let headers = new Headers({ 'Content-Type': 'application/json' });
         this.http.post("/api/account/login", body, { headers: headers })
             .map(res => res.json())
             .subscribe(data => {
-				console.log(data);
-		        this.router.navigate(["/example"]);
-	        });
+                console.log(data);
+                this.router.navigate(["/example"]);
+            }, data1 => {
+                if (data1.status === 401) {
+                    this.showLoginUnauthorizedDiv = true;
+                }
+            });
     }
 
     onRegisterSubmit(form: NgForm) {
+        this.showRegisterSuccessDiv = false;
         var body = JSON.stringify(this.registerModel);
         let headers = new Headers({ 'Content-Type': 'application/json' });
 
         this.http.post("/api/account/register", body, { headers: headers })
             .map(res => res.json())
 			.subscribe(data => {
-				console.log(data);
+                console.log(data);
+                this.showRegisterSuccessDiv = true;
 	        });
     }
 }
