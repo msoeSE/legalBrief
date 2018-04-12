cdffcd339c463438c27135082ef16380999303fa
jacobgralinski <jacobgralinski@gmail.com>
2018-04-11 23:29:28 -0500

    Fixed filename typo

--- BriefAssistant.Tests/conf.js
+++ BriefAssistant.Tests/conf.js
@@ -1,6 +1,6 @@
 ﻿exports.config = {
     //seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['initial-form-validation.spec.js', 'registration-validation-spec.js'],
+    specs: ['initial-form-validation-spec.js', 'registration-validation-spec.js'],
     framework: 'jasmine2',
 
     capabilities: {
