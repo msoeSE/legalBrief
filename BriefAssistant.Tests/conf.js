63be147ae060034f04000e93334aab126c3a3cc1
jacobgralinski <jacobgralinski@gmail.com>
2018-04-18 17:10:13 -0500

    Created Protractor tests for the reply form and response forms.

--- BriefAssistant.Tests/conf.js
+++ BriefAssistant.Tests/conf.js
@@ -1,6 +1,6 @@
 ﻿exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['form-validation-spec.js', 'registration-validation-spec.js'],
+    specs: ['form-validation-spec.js', 'registration-validation-spec.js', 'reply-form-validation-spec.js', 'response-form-validation-spec.js'],
     //specs: ['registration-validation-spec.js'],
     framework: 'jasmine2',
 
