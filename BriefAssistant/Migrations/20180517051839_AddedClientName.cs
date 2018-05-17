e96402d1b9294e267af9fba214d7580c45fa1260
calharden <calharden@gmail.com>
2018-05-17 00:20:01 -0500

    Added migrations

---
+++ BriefAssistant/Migrations/20180517051839_AddedClientName.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BriefAssistant.Migrations
+{
+    public partial class AddedClientName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientName",
+                table: "Cases",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientName",
+                table: "Cases");
+        }
+    }
+}
